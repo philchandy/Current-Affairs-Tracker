f18146cff6f434e3ad896ea210cfed246ef0bac7
Phil Chandy <phillipchandy200@gmail.com>
2024-10-05 20:23:01 -0400

    fix update data logic

--- server/server.js
+++ server/server.js
@@ -8,14 +8,30 @@ const {
 } = require('./dataHandler');
 const path = require('path');
 
+const tempDir = path.join(__dirname, 'tempPDFs');
+
 const app = express();
 const PORT = 3001;
 
 app.use(cors());
 
-const isTempFolderEmpty = (tempDir) => {
-    return fs.existsSync(tempDir) && fs.readdirSync(tempDir).length === 0;
+// Function to check if the temp folder exists and create it if it doesn't
+const checkAndCreateTempFolder = () => {
+    if (!fs.existsSync(tempDir)) {
+        fs.mkdirSync(tempDir);
+        console.log(`Created directory: ${tempDir}`);
+    } else {
+        console.log(`Directory already exists: ${tempDir}`);
+    }
 };
+
+// Function to check if the temp folder is empty
+const isTempFolderEmpty = () => {
+    return fs.readdirSync(tempDir).length === 0;
+};
+
+// Check and create the temp folder on server start
+checkAndCreateTempFolder();
 
 // Route to trigger data refresh
 app.post('/api/refresh', async (req, res) => {
